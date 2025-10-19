1094c7170981aaa9897d67e2564bd925fa5e4907
ferreiraalan13 <ferreiraalan13@gmail.com>
2025-10-19 10:47:52 -0300

    ajuste do banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,6 +1,20 @@
-import banner from "../../assets/banner.png";
+import banner from "../../assets/banner.jpg";
 import styles from "./Banner.module.scss";
 
 export function Banner() {
-  return <img className={styles.banner} src={banner} alt="banner" />;
+  return (
+    <div className={styles.bannerContainer}>
+      <img className={styles.banner} src={banner} alt="banner" />
+      <div className={styles.bannerContent}>
+        <h1 className={styles.title}>
+          Venha conhecer nossas <br />
+          promoções
+        </h1>
+        <p className={styles.subtitle}>
+          <span className={styles.offText}>50% Off</span> nos produtos
+        </p>
+        <button className={styles.button}>Ver produto</button>
+      </div>
+    </div>
+  );
 }
